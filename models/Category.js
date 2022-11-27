2f1e8cb15814440958bf98153f350c8bac7f88ab
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-11-27 13:44:48 +0300

    27.11-c2

---
+++ models/Category.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.schema;
+
+const CategorySchema = new Schema(
+  {
+    title: {
+      type: String,
+      minlegth: [3, "Title must be 3 characters longer"],
+      trim: true,
+      unique: true,
+      uniqueCaseInsensetive: true,
+      required: [true, "Title is required"],
+    },
+    description: {
+      type: String,
+      minlegth: [3, "Title must be 3 characters longer"],
+      required: [true, "Title is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+CategorySchema.plugin(uniqueValidator, { message: "{PATH} already exists" });
+
+module.exports = mongoose.model("Category", CategorySchema);
