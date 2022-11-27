d4860b4e1be301fd84998761178ae5be77b3a929
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-11-27 16:49:26 +0300

    27.11-c4

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,7 +1,44 @@
 const Category = require("./../models/Category.js");
 
+// @desc  Get categories
+// @route GET api/v1/categories
+// @access private/admin
+
 exports.getCategories = async (req, res, next) => {
   const categories = await Category.find();
-
   res.status(200).json({ succes: true, data: categories });
+};
+
+// @desc  Post Create Categories
+// @route POST api/v1/categories
+// @access private/admin
+
+exports.createCategories = async (req, res, next) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(200).json({ succes: true, data: category });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
+};
+
+// @desc  Get category
+// @route GET api/v1/categories/:id
+// @access private/admin
+
+exports.getCategory = async (req, res, next) => {
+  try {
+    const category = await Category.findById(req.params.id);
+    if (!category) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          error: `No category with that id of ${req.params.id}`,
+        });
+    }
+    res.status(200).json({ succes: true, data: category });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
 };
--- routes/categories.js
+++ routes/categories.js
@@ -4,6 +4,9 @@ const categoryController = require("./../controllers/categories");
 
 const router = express.Router();
 
-router.route("/").get(categoryController.getCategories);
-
+router
+  .route("/")
+  .get(categoryController.getCategories)
+  .post(categoryController.createCategories);
+router.route("/:id").get(categoryController.getCategory);
 module.exports = router;
